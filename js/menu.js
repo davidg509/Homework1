cd682943a1cc14dbe61c954f884a194aae7947ed
unknown <iguest@is-m440m27-32.ischool.uw.edu>
2013-11-06 14:11:04 -0800

    HW3

---
+++ js/menu.js
@@ -0,0 +1,50 @@
+
+
+
+
+
+function render(pizzas, drinks, desserts){
+	var idx;
+	var pizza;
+
+	pizzas=com.dawgpizza.menu.pizzas; // (Pizza array. com.pizza.menu is the total object. Three arrays within this object)
+	drink=com.dawgpizza.menu.pizza;
+	dessert=com.dawgpizza.menu.pizza;
+
+	var pizzaVariable = $('.templatePizza');
+
+	for (idx = 0; idx < com.dawgpizza.menu.pizzas.length; ++idx) {
+	    pizza = com.dawgpizza.menu.pizzas[idx];
+
+	    var clone=pizzaVariable.clone();
+	    clone.find('.name').html(pizza.name);
+	    clone.find('.description').html(pizza.description);
+	    clone.find('.prices').html("$" + pizza.prices[0] + "/" + "$" + pizza.prices[1] + "/" + "$" + pizza.prices[2]);
+
+	    $('div.pizzaheading').append(clone);
+
+	}
+
+	for (idx = 0; idx < com.dawgpizza.menu.drinks.length; ++idx) {
+	    drinks = com.dawgpizza.menu.drinks[idx];
+
+	    var clone=pizzaVariable.clone();
+	    clone.find('.name').html(drinks.name);
+	    clone.find('.prices').html(drinks.price);
+	    $('div.drinksheading').append(clone);
+	}
+	for (idx = 0; idx < com.dawgpizza.menu.desserts.length; ++idx) {
+	    desserts = com.dawgpizza.menu.desserts[idx];
+
+	    var clone=pizzaVariable.clone();
+	    clone.find('.name').html(desserts.name);
+	    clone.find('.prices').html(desserts.price);
+	    $('div.dessertheading').append(clone);
+	}
+}
+
+
+
+$(function(){
+	render(com.dawgpizza.menu.pizzas, com.dawgpizza.menu.drinks, com.dawgpizza.menu.dessert);
+});
